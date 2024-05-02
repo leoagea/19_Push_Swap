625342f3ae03aa3b804c3154739087fcdf058531
leoagea <leoagea@gmail.com>
2024-05-02 13:29:36 +0200

    change content name to data

--- inc/libft.h
+++ inc/libft.h
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 01:26:11 by lagea             #+#    #+#             */
-/*   Updated: 2024/05/02 13:17:28 by lagea            ###   ########.fr       */
+/*   Updated: 2024/05/02 13:29:02 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,6 +122,6 @@ void	ft_putunbr_fd(unsigned int n, int fd);
 
 /*------------------------------------DOUBLE LIMKED LIST---------------------------------------*/
 
-t_node	*dll_new_node(void *content);
+t_node	*dll_new_node(ssize_t data);
 
 #endif
--- libft/src/ddl_new_node.c
+++ libft/src/ddl_new_node.c
@@ -6,21 +6,21 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 12:23:41 by lagea             #+#    #+#             */
-/*   Updated: 2024/05/02 13:17:18 by lagea            ###   ########.fr       */
+/*   Updated: 2024/05/02 13:29:07 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/libft.h"
 #include "../../inc/push_swap.h"
 
-t_node	*dll_new_node(void *content)
+t_node	*dll_new_node(ssize_t data)
 {
 	t_node *new;
 
 	new = malloc(sizeof(t_node));
 	if (!new)
 		return (NULL);
-	new->value = content;
+	new->value = data;
 	new->next = NULL;
 	new->prev = NULL;
 	return (new);
