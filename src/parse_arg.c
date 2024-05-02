274ae32a0380f0fb7ae7a66d1819c8779e3d67e8
leoagea <leoagea@gmail.com>
2024-05-02 11:55:55 +0200

    add funct get char** len OK

--- src/parse_arg.c
+++ src/parse_arg.c
@@ -6,12 +6,29 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 11:29:30 by lagea             #+#    #+#             */
-/*   Updated: 2024/05/02 11:43:50 by lagea            ###   ########.fr       */
+/*   Updated: 2024/05/02 11:53:32 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/libft.h"
 #include "../inc/push_swap.h"
+
+size_t d_array_len (char **lst)
+{
+	size_t i;
+	size_t len;
+	
+	i = 0;
+	len = 0;
+	if(!lst)
+		return 0;
+	while (lst[i] != NULL)
+	{
+		len++;
+		i++;
+	}
+	return len;
+}
 
 char **parse_arg(int ac, char **av)
 {
@@ -20,10 +37,12 @@ char **parse_arg(int ac, char **av)
 	{
 		lst = ft_split(av[1],' ');
 	}
-	for (int i = 0;i < 10;i++)
+	for (int i = 0;i < 12;i++)
 		printf("%s\n",lst[i]);
+	printf("\n%zu\n",d_array_len(lst));
 	return lst;
 }
+
 
 int main(int ac,char **av)
 {
