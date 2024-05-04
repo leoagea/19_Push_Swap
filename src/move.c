5a5b2a6324f5ec0773e09c2be7da0f81a6d22bbe
leoagea <leoagea@gmail.com>
2024-05-04 20:56:09 +0200

    add if OK do nothing if stack size = 2 or 1

--- src/move.c
+++ src/move.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 20:44:30 by lagea             #+#    #+#             */
-/*   Updated: 2024/05/04 20:49:51 by lagea            ###   ########.fr       */
+/*   Updated: 2024/05/04 20:55:32 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void move_sa(t_stack *stack)
 {
 	t_node *temp;
 
+	if (dll_size(stack->a) <= 2)
+		return ;
 	temp = dll_new_node(stack->a->head->value);
 	stack->a->head->value = stack->a->head->next->value;
 	stack->a->head->next->value = temp->value;
