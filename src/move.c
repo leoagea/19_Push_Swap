6375095a9f78ed1915666d73bef679f767aec83a
leoagea <leoagea@gmail.com>
2024-05-06 17:50:28 +0200

    update moves to update index

--- src/move.c
+++ src/move.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 20:44:30 by lagea             #+#    #+#             */
-/*   Updated: 2024/05/06 14:17:32 by lagea            ###   ########.fr       */
+/*   Updated: 2024/05/06 17:43:59 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void swap_a(t_stack *stack)
 	temp = dll_new_node(stack->a->head->value);
 	stack->a->head->value = stack->a->head->next->value;
 	stack->a->head->next->value = temp->value;
+	index_init(stack->a);
 	free(temp);
 }
 
@@ -34,6 +35,7 @@ void swap_b(t_stack *stack)
 	temp = dll_new_node(stack->b->head->value);
 	stack->b->head->value = stack->b->head->next->value;
 	stack->b->head->next->value = temp->value;
+	index_init(stack->b);
 	free(temp);
 }
 
@@ -54,6 +56,7 @@ void push_a(t_stack *stack)
 	temp = stack->b->head;
 	dll_insert_head(temp->value,stack->a);
 	stack->b->head = temp->next;
+	index_init_stack(stack);
 	free(temp);
 }
 
@@ -66,5 +69,6 @@ void push_b(t_stack *stack)
 	temp = stack->a->head;
 	dll_insert_head(temp->value,stack->b);
 	stack->a->head = temp->next;
+	index_init_stack(stack);
 	free(temp);
 }
