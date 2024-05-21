0af34e18634a6a60d7780954a3b6f69bae613df9
leoagea <leoagea@gmail.com>
2024-05-21 15:21:57 +0200

    Update : rotate and rev_rotate check if the stack where we apply the move is not empty if it is free both stack, write error and exit

---
+++ bonus/reverse_rotate.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 00:50:04 by lagea             #+#    #+#             */
+/*   Updated: 2024/05/21 15:17:26 by lagea            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/libft.h"
+#include "../inc/push_swap.h"
+
+void	reverse_rotate_a(t_stack *stack_a, t_stack *stack_b, bool print)
+{
+	ssize_t	temp;
+
+	if (stack_a->head == NULL)
+		exit_error_cmd(stack_a, stack_b);
+	temp = stack_a->tail->value;
+	dll_delete_tail(stack_a);
+	dll_insert_head(temp, stack_a);
+	index_init(stack_a);
+	if (print)
+		write(1, "rra\n", 4);
+}
+
+void	reverse_rotate_b(t_stack *stack_b, t_stack *stack_a, bool print)
+{
+	ssize_t	temp;
+
+	if (stack_b->head == NULL)
+		exit_error_cmd(stack_a, stack_b);
+	temp = stack_b->tail->value;
+	dll_delete_tail(stack_b);
+	dll_insert_head(temp, stack_b);
+	index_init(stack_b);
+	if (print)
+		write(1, "rrb\n", 4);
+}
+
+void	reverse_rotate_rr(t_stack *stack_a, t_stack *stack_b)
+{
+	reverse_rotate_a(stack_a, stack_b, false);
+	reverse_rotate_b(stack_b, stack_a, false);
+	write(1, "rrr\n", 4);
+}
---
+++ bonus/rotate.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 00:37:33 by lagea             #+#    #+#             */
+/*   Updated: 2024/05/21 15:17:45 by lagea            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/libft.h"
+#include "../inc/push_swap.h"
+
+void	rotate_a(t_stack *stack_a, t_stack *stack_b, bool print)
+{
+	ssize_t	temp;
+
+	if (stack_a->head == NULL)
+		exit_error_cmd(stack_a, stack_b);
+	temp = stack_a->head->value;
+	dll_delete_head(stack_a);
+	dll_insert_tail(temp, stack_a);
+	index_init(stack_a);
+	if (print)
+		write(1, "ra\n", 3);
+}
+
+void	rotate_b(t_stack *stack_b, t_stack *stack_a, bool print)
+{
+	ssize_t	temp;
+
+	if (stack_b->head == NULL)
+		exit_error_cmd(stack_a, stack_b);
+	temp = stack_b->head->value;
+	dll_delete_head(stack_b);
+	dll_insert_tail(temp, stack_b);
+	index_init(stack_b);
+	if (print)
+		write(1, "rb\n", 3);
+}
+
+void	rotate_rr(t_stack *stack_a, t_stack *stack_b)
+{
+	rotate_a(stack_a, stack_b, false);
+	rotate_b(stack_b, stack_a, false);
+	write(1, "rr\n", 3);
+}
