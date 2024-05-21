3bf883cc6a2c32b80e624f47621a0110cfd3ce97
leoagea <leoagea@gmail.com>
2024-05-21 15:23:32 +0200

    Add : funct exit_error_cmd function to free both stack, write error and exit

--- bonus/exit.c
+++ bonus/exit.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 15:05:02 by lagea             #+#    #+#             */
-/*   Updated: 2024/05/20 18:28:27 by lagea            ###   ########.fr       */
+/*   Updated: 2024/05/21 15:13:40 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,12 @@ void	exit_stack(t_stack *stack_a, t_stack *stack_b)
 	dll_clear(stack_a);
 	dll_clear(stack_b);
 	exit(EXIT_SUCCESS);
+}
+
+void exit_error_cmd(t_stack *stack_a, t_stack *stack_b)
+{
+	dll_clear(stack_a);
+	dll_clear(stack_b);
+	write(1, "Error\n", 6);
+	exit(EXIT_FAILURE);
 }
