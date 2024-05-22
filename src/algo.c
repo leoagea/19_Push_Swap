30ddd2781785e4b81d4f8a9c4e26767e0045ffb6
leoagea <leoagea@gmail.com>
2024-05-22 16:41:49 +0200

    Update : add protect if stack->a->head is NULL

--- src/algo.c
+++ src/algo.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 12:56:47 by lagea             #+#    #+#             */
-/*   Updated: 2024/05/22 15:22:14 by lagea            ###   ########.fr       */
+/*   Updated: 2024/05/22 16:31:48 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	push_swap(t_stack *stack_a, t_stack *stack_b)
 	t_node	*min_cost;
 	t_node	*bestfriend;
 	
+	if (stack_a->head == NULL)
+		return ;
 	if (dll_size(stack_a) == 1)
 		return ;
 	else if (dll_size(stack_a) == 2)
@@ -37,10 +39,6 @@ void	push_swap(t_stack *stack_a, t_stack *stack_b)
 		sort_five_elements(stack_a, stack_b);
 		sort(stack_a, stack_b);
 		final_rotate(stack_a, stack_b);
-		// printf("stack B\n");
-		// print_stack(stack_b);
-		// printf("\nstack A\n");
-		// print_stack(stack_a);
 	}
 }
 
