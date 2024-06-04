ac9509f4b1ca0863251e08b983de311b01e1dc3c
leoagea <leoagea@gmail.com>
2024-06-04 17:44:19 +0200

    Update : add protec is_sort if stack_a is NULL return false

--- bonus/checker_bonus.c
+++ bonus/checker_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 12:22:51 by lagea             #+#    #+#             */
-/*   Updated: 2024/06/04 15:00:57 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/04 17:42:17 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ bool	is_sort(t_stack *stack_a)
 {
 	t_node	*current;
 
+	if (stack->a == NULL)
+		return (false);
 	current = stack_a->head;
 	while (current->next != NULL)
 	{
